9b71657bb2108f93ff15730273612ccff76d6b77
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2024-11-23 11:58:21 +0100

    Add comment to clarify process.exit(0) (#161)
    Copied from https://github.com/actions/cache/pull/1217

--- src/save-cache.ts
+++ src/save-cache.ts
@@ -20,6 +20,11 @@ export async function run(): Promise<void> {
     const err = error as Error;
     core.setFailed(err.message);
   }
+  // node will stay alive if any promises are not resolved,
+  // which is a possibility if HTTP requests are dangling
+  // due to retries or timeouts. We know that if we got here
+  // that all promises that we care about have successfully
+  // resolved, so simply exit with success.
   process.exit(0);
 }
 
