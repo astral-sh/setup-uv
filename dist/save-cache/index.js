astral-sh/setup-uv

commit e3fb95a68959a9be3d650989dc1d43a9a324447a

Warn instead of fail for no-dependency-glob (#200)
Closes: #199

dist/save-cache/index.js
@@ -91490,11 +91490,9 @@ function computeKeys(version) {
             core.info(`Searching files using cache dependency glob: ${inputs_1.cacheDependencyGlob.split("\n").join(",")}`);
             cacheDependencyPathHash += yield (0, hash_files_1.hashFiles)(inputs_1.cacheDependencyGlob, true);
             if (cacheDependencyPathHash === "-") {
-                throw new Error(`No file matched to [${inputs_1.cacheDependencyGlob.split("\n").join(",")}], make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`);
+                core.warning(`No file matched to [${inputs_1.cacheDependencyGlob.split("\n").join(",")}]. The cache will never get invalidated. Make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`);
             }
-        }
-        else {
-            cacheDependencyPathHash += "no-dependency-glob";
+            cacheDependencyPathHash = "-no-dependency-glob";
         }
         const suffix = inputs_1.cacheSuffix ? `-${inputs_1.cacheSuffix}` : "";
         const pythonVersion = yield getPythonVersion();

dist/setup/index.js
@@ -96573,11 +96573,9 @@ function computeKeys(version) {
             core.info(`Searching files using cache dependency glob: ${inputs_1.cacheDependencyGlob.split("\n").join(",")}`);
             cacheDependencyPathHash += yield (0, hash_files_1.hashFiles)(inputs_1.cacheDependencyGlob, true);
             if (cacheDependencyPathHash === "-") {
-                throw new Error(`No file matched to [${inputs_1.cacheDependencyGlob.split("\n").join(",")}], make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`);
+                core.warning(`No file matched to [${inputs_1.cacheDependencyGlob.split("\n").join(",")}]. The cache will never get invalidated. Make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`);
             }
-        }
-        else {
-            cacheDependencyPathHash += "no-dependency-glob";
+            cacheDependencyPathHash = "-no-dependency-glob";
         }
         const suffix = inputs_1.cacheSuffix ? `-${inputs_1.cacheSuffix}` : "";
         const pythonVersion = yield getPythonVersion();

src/cache/restore-cache.ts
@@ -43,12 +43,11 @@ async function computeKeys(version: string): Promise<string> {
     );
     cacheDependencyPathHash += await hashFiles(cacheDependencyGlob, true);
     if (cacheDependencyPathHash === "-") {
-      throw new Error(
-        `No file matched to [${cacheDependencyGlob.split("\n").join(",")}], make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`,
+      core.warning(
+        `No file matched to [${cacheDependencyGlob.split("\n").join(",")}]. The cache will never get invalidated. Make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`,
       );
     }
-  } else {
-    cacheDependencyPathHash += "no-dependency-glob";
+    cacheDependencyPathHash = "-no-dependency-glob";
   }
   const suffix = cacheSuffix ? `-${cacheSuffix}` : "";
   const pythonVersion = await getPythonVersion();

commit 180f8b44399608a850e1db031fa65c77746566d3

Fix wrong cacheDependencyPathHash (#201)
Introduced in https://github.com/astral-sh/setup-uv/pull/200 and not
caught because the test is not needed for automerge to pass

dist/save-cache/index.js
@@ -91492,6 +91492,8 @@ function computeKeys(version) {
             if (cacheDependencyPathHash === "-") {
                 core.warning(`No file matched to [${inputs_1.cacheDependencyGlob.split("\n").join(",")}]. The cache will never get invalidated. Make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`);
             }
+        }
+        if (cacheDependencyPathHash === "-") {
             cacheDependencyPathHash = "-no-dependency-glob";
         }
         const suffix = inputs_1.cacheSuffix ? `-${inputs_1.cacheSuffix}` : "";

dist/setup/index.js
@@ -96575,6 +96575,8 @@ function computeKeys(version) {
             if (cacheDependencyPathHash === "-") {
                 core.warning(`No file matched to [${inputs_1.cacheDependencyGlob.split("\n").join(",")}]. The cache will never get invalidated. Make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`);
             }
+        }
+        if (cacheDependencyPathHash === "-") {
             cacheDependencyPathHash = "-no-dependency-glob";
         }
         const suffix = inputs_1.cacheSuffix ? `-${inputs_1.cacheSuffix}` : "";

src/cache/restore-cache.ts
@@ -47,6 +47,8 @@ async function computeKeys(version: string): Promise<string> {
         `No file matched to [${cacheDependencyGlob.split("\n").join(",")}]. The cache will never get invalidated. Make sure you have checked out the target repository and configured the cache-dependency-glob input correctly.`,
       );
     }
+  }
+  if (cacheDependencyPathHash === "-") {
     cacheDependencyPathHash = "-no-dependency-glob";
   }
   const suffix = cacheSuffix ? `-${cacheSuffix}` : "";