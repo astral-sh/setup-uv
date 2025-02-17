e2e90872575c605cafef44670a41d7c6465b8d2f
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2025-02-17 10:32:34 +0100

    Support OS using musl (#284)
    Fixes: #278

--- dist/save-cache/index.js
+++ dist/save-cache/index.js
@@ -91494,7 +91494,8 @@ async function computeKeys() {
     }
     const suffix = inputs_1.cacheSuffix ? `-${inputs_1.cacheSuffix}` : "";
     const pythonVersion = await getPythonVersion();
-    return `setup-uv-${CACHE_VERSION}-${(0, platforms_1.getArch)()}-${(0, platforms_1.getPlatform)()}-${pythonVersion}${cacheDependencyPathHash}${suffix}`;
+    const platform = await (0, platforms_1.getPlatform)();
+    return `setup-uv-${CACHE_VERSION}-${(0, platforms_1.getArch)()}-${platform}-${pythonVersion}${cacheDependencyPathHash}${suffix}`;
 }
 async function getPythonVersion() {
     if (inputs_1.pythonVersion !== "") {
@@ -91847,13 +91848,48 @@ function expandTilde(input) {
 /***/ }),
 
 /***/ 8361:
-/***/ ((__unused_webpack_module, exports) => {
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getArch = getArch;
 exports.getPlatform = getPlatform;
+const exec = __importStar(__nccwpck_require__(5236));
+const core = __importStar(__nccwpck_require__(7484));
 function getArch() {
     const arch = process.arch;
     const archMapping = {
@@ -91867,15 +91903,46 @@ function getArch() {
         return archMapping[arch];
     }
 }
-function getPlatform() {
-    const platform = process.platform;
+async function getPlatform() {
+    const processPlatform = process.platform;
     const platformMapping = {
         linux: "unknown-linux-gnu",
         darwin: "apple-darwin",
         win32: "pc-windows-msvc",
     };
-    if (platform in platformMapping) {
-        return platformMapping[platform];
+    if (processPlatform in platformMapping) {
+        const platform = platformMapping[processPlatform];
+        if (platform === "unknown-linux-gnu") {
+            const isMusl = await isMuslOs();
+            return isMusl ? "unknown-linux-musl" : platform;
+        }
+        return platform;
+    }
+}
+async function isMuslOs() {
+    let stdOutput = "";
+    let errOutput = "";
+    const options = {
+        silent: !core.isDebug(),
+        listeners: {
+            stdout: (data) => {
+                stdOutput += data.toString();
+            },
+            stderr: (data) => {
+                errOutput += data.toString();
+            },
+        },
+        ignoreReturnCode: true,
+    };
+    try {
+        const execArgs = ["--version"];
+        await exec.exec("ldd", execArgs, options);
+        return stdOutput.includes("musl") || errOutput.includes("musl");
+    }
+    catch (error) {
+        const err = error;
+        core.warning(`Failed to determine glibc or musl. Falling back to glibc. Error: ${err.message}`);
+        return false;
     }
 }
 
