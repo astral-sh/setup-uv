29cd2350cd44d155ed44d0eba0e1b63d07fb3b69
Christoph Reiter <reiter.christoph@gmail.com>
2025-10-24 16:13:48 +0200

    Use tar for extracting the uv zip file on Windows too (#660)
    Use extractTar() instead of extractZip() which is very slow for some
    reason (0.3s vs 10s)
    
    Fixes #659

--- src/download/download-version.ts
+++ src/download/download-version.ts
@@ -108,12 +108,21 @@ async function downloadVersion(
   await validateChecksum(checkSum, downloadPath, arch, platform, version);
 
   let uvDir: string;
-  const extension = getExtension(platform);
   if (platform === "pc-windows-msvc") {
-    const fullPathWithExtension = `${downloadPath}${extension}`;
-    await fs.copyFile(downloadPath, fullPathWithExtension);
-    uvDir = await tc.extractZip(fullPathWithExtension);
     // On windows extracting the zip does not create an intermediate directory
+    try {
+      // Try tar first as it's much faster, but only bsdtar supports zip files,
+      // so this my fail if another tar, like gnu tar, ends up being used.
+      uvDir = await tc.extractTar(downloadPath, undefined, "x");
+    } catch (err) {
+      core.info(
+        `Extracting with tar failed, falling back to zip extraction: ${(err as Error).message}`,
+      );
+      const extension = getExtension(platform);
+      const fullPathWithExtension = `${downloadPath}${extension}`;
+      await fs.copyFile(downloadPath, fullPathWithExtension);
+      uvDir = await tc.extractZip(fullPathWithExtension);
+    }
   } else {
     const extractedDir = await tc.extractTar(downloadPath);
     uvDir = path.join(extractedDir, artifactName);
